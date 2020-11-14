e60d163159cbcf61244a39a7f039a360899fef02
Skelatorian <mylynhle21@gmail.com>
2020-11-13 22:02:21 -0500

    attach server

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const { endsWith } = require("sequelize/types/lib/operators")
+//const { endsWith } = require("sequelize/types/lib/operators")
 
-testesttestestedtset
-endsWiths
+
+const express = require('express');
+const express_hbs = require('express-handlebars');
+const path = require('path');
+//const db = require('./config/connection');
+
+
+
+
+const app = express();
+app.get('/', (req,res) => {
+    res.send('Test');
+});
+
+const PORT = process.env.Port || 8080;
+
+app.listen(PORT, console.log(`listening on port ${PORT}`));
