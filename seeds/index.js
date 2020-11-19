4252de99e687d4fe7346384b66bd2c74d840921d
aznjp <jpark103193@gmail.com>
2020-11-18 23:05:47 -0500

    removed the post seeds

--- seeds/index.js
+++ seeds/index.js
@@ -7,7 +7,7 @@ const sequelize = require('../config/connection');
 const seedAll = async() => {
     await sequelize.sync({ force: true });
     console.log('--------------');
-  
+
     await seedUsers();
     console.log('--------------');
 
@@ -16,9 +16,6 @@ const seedAll = async() => {
     console.log('--------------');
 
     await seedComments();
-    console.log('--------------');
-
-    await seedVotes();
     console.log('--------------');
 
     process.exit(0);
