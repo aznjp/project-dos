7f83ac5487ddf6291c19a0bc239e21547e165bbf
Skelatorian <mylynhle21@gmail.com>
2020-11-22 22:01:07 -0500

    fixed promise rejections

--- public/javascript/edit-recipe.js
+++ public/javascript/edit-recipe.js
@@ -1,11 +1,20 @@
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
 async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="recipe-title"]').value.trim();
     const ingredients = document.querySelector('textarea[name="ingredients"]').value;
     const instructions = document.querySelector('textarea[name="instructions"]').value;
-    const recipe_image = document.getElementById("blah").src
-
+    const file = document.querySelector('input[name="recipe-img"]').files[0];
+    console.log(file);
+    const recipe_image = file ? (await toBase64(file)) : undefined;
+    console.log(recipe_image);
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
