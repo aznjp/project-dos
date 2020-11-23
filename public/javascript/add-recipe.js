0ff072eea9b814f019ab3afd45d95497560e6119
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-11-22 19:41:35 -0500

    fixing br points

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,13 +1,13 @@
-const { urlencoded } = require("express");
-
 async function newRecipeHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="recipe-title"]').value;
-    const ingredients = document.querySelector('textarea[name="ingredients"]').value;
-    const instructions = document.querySelector('textarea[name="instructions"]').value;
-    const recipe_image = document.getElementById("blah").src;
-    console.log(recipe_image)
+    const preParseIngredients = document.querySelector('textarea[name="ingredients"]').value;
+    const preParseInstructions = document.querySelector('textarea[name="instructions"]').value;
+    const recipe_image = document.getElementById("blah").src
+    const ingredients = preParseIngredients.replace(/(\r\n|\n|\r)/gm, "<br/>");
+    const instructions = preParseInstructions.replace(/(\r\n|\n|\r)/gm, "<br/>");
+
     const response = await fetch(`/api/recipes`, {
         method: 'POST',
         body: JSON.stringify({
@@ -20,7 +20,6 @@ async function newRecipeHandler(event) {
             'Content-Type': 'application/json'
         }
     });
-    console.log(response)
     if (response.ok) {
         document.location.replace('/dashboard');
     } else {
