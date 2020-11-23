9f29bf250a481726e5fc412ff2b79a17ce8383ed
Skelatorian <mylynhle21@gmail.com>
2020-11-22 22:03:58 -0500

    multer config

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,12 +1,19 @@
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
 async function newRecipeHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="recipe-title"]').value;
     const ingredients = document.querySelector('textarea[name="ingredients"]').value;
     const instructions = document.querySelector('textarea[name="instructions"]').value;
-    const recipe_image = document.getElementById("blah").src
+    const file = document.querySelector('input[name="recipe-img"]').files[0];
+    const recipe_image = file ? (await toBase64(file)) : undefined
 
-    console.log(recipe_image)
     const response = await fetch(`/api/recipes`, {
         method: 'POST',
         body: JSON.stringify({
