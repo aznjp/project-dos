ebcd6b13e6723e96d252682070dbd0c1ce62cf29
aznjp <jpark103193@gmail.com>
2020-11-28 22:53:53 -0500

    cleaned up and refactored code

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,14 +1,5 @@
 async function newRecipeHandler(event) {
     event.preventDefault();
-    console.log(event)
-        // const title = document.querySelector('input[name="recipe-title"]').value;
-        // const preParseIngredients = document.querySelector('textarea[name="ingredients"]').value;
-        // const preParseInstructions = document.querySelector('textarea[name="instructions"]').value;
-        // const recipe_image = document.querySelector('input[name="recipe-img"]').files[0].name
-        //     // const recipe_image = document.getElementById("blah").src
-        // const ingredients = preParseIngredients.replace(/(\r\n|\n|\r)/gm, "<br/>");
-        // const instructions = preParseInstructions.replace(/(\r\n|\n|\r)/gm, "<br/>");
-
     let form = document.querySelector("#recipe-form")
     let formData = new FormData(form)
 
